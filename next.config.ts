1dffbef3d32b44ad86e4a3440c072d96a4179c9f
Snizhana <snizhanapetrushka@gmail.com>
2026-09-17 15:28:56 +0200

    fix (config): allow Cloudinary images in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
-  async redirects() {
-    return [
-      {
-        source: "/about",
-        destination: "/",
-        permanent: true,
-      },
-    ];
-  },
-
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'ac.goit.global',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ac.goit.global",
       },
     ],
   },
-  
 };
 
 export default nextConfig;
